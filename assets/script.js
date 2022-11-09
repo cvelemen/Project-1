a4b8f307e5d96339b1d3286bfbe4d027704bad56
fbdthree <112531356+fbdthree@users.noreply.github.com>
2022-11-09 18:48:43 -0500

    second js commit

--- assets/script.js
+++ assets/script.js
@@ -8,64 +8,87 @@
 // TODO: randomly find a cocktail from the array based on the weather
 // // create arrays that hold cocktails by weather category
 sunnyCocktails = [
-  aperol_spritz,
-  caipirinha,
-  daiquiri,
-  mai_tai,
-  margarita,
-  mint_julep,
-  mojito,
-  moscow - mule,
-  tom_collins,
-  pina_colada
+  "aperol_spritz",
+  "caipirinha",
+  "daiquiri",
+  "mai_tai",
+  "margarita",
+  "mint_julep",
+  "mojito",
+  "moscow - mule",
+  "tom_collins",
+  "pina_colada"
 ]
 
 rainyCocktails = [
-  cosmopolitan,
-  espresso_martini,
-  last_word,
-  old_fashioned,
-  manhattan,
-  martinez,
-  martini,
-  negroni,
-  rusty_nail,
-  vesper
+  "cosmopolitan",
+  "espresso_martini",
+  "last_word ",
+  "old_fashioned",
+  "manhattan",
+  "martinez",
+  "martini",
+  "negroni",
+  "rusty_nail",
+  "vesper"
 ]
 
 snowyCocktails = [
-  hot_creamy_bush,
-  hot_toddy,
-  irish_coffee,
-  nutty_irishman,
-  orange_scented_hot_chocolate,
-  rum_toddy,
-  spanish_chocolate,
-  snowday,
-  swedish_coffee,
-  talos_coffee
+  "hot_creamy_bush",
+  "hot_toddy",
+  "irish_coffee",
+  "nutty_irishman",
+  "orange_scented_hot_chocolate",
+  "rum_toddy",
+  "spanish_chocolate",
+  "snowday",
+  "swedish_coffee",
+  "talos_coffee"
 ]
 
 
-function getCocktailAPI() {
-  fetch('www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+var randomSelect = Math.floor(Math.random() * snowyCocktails.length)
+var drink = snowyCocktails[randomSelect]
+
+getCocktailAPI(drink)
+function getCocktailAPI(drink) {
+  console.log(`https://thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+  fetch(`https://thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
     .then((response) => {
-      if (!response.ok) { // check for !falsy values
-        console.table(response)
-        document.body.append('response came back not ok!')
-        document.query
-      }
-      return response.json()
+              return response.json()
     }).then((data) => {
-      console.log(data);
-      return data;
+      console.log("API", data);
+      var html = `<div class="card">
+      <div class="card-image">
+        <figure class="image is-4by3">
+          <img src="${data.drinks[0].strDrinkThumb}"alt="Placeholder image">
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+      
+            <figure class="image is-48x48"></figure>
+          
+          <div class="media-content">
+            <p class="title is-5">${data.drinks[0].strDrink}}</p>
+            <p class="subtitle is-6">@johnsmith</p>
+          </div>
+        </div>
+    
+        <div class="content">
+          Cocktail photo, ingredients, and technique
+          <ul>
+            <li></li>
+          </ul>
+      </div>
+      </div>
+    </div>`
+    document.getElementById("cocktailChoice").innerHTML = html
+     
     }).catch((err) => {
       console.error(err)
-      const errorElement = document.createElement("p")
-      errorElement.textContent = err.toString() || "500 Internal Server Error at The Cocktail DB"
-      document.body.append(errorElement)
     })
-    .finally(() => console.info('I will always run in the finally block'))
+   
 }
 
 // // var randomDrinkNum = drinksByWeather.currentWeather.length * randomNumberExpressionHERE
